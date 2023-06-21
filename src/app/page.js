c8a18d7d79f2fa3f862b0cbe5f039b7a50ae6f61
sagar <anandguptasir@gmail.com>
2023-06-21 16:12:57 +0530

    change commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
+import { checkout } from '../checkout';
+
 
 export default function Home() {
   return (
   <main className={styles.main}>
     <div>
-    <Image className={styles.img}  src="/vercel.svg" width={100} height={50}/>
-    <button className={styles.button}>Buy This image</button>
+    <Image alt='img' className={styles.img}  src="/vercel.svg" width={100} height={50}/>
+    <button className={styles.button}
+     onClick={(()=>{
+      checkout(
+        {
+          lineItems:[{price:"price_1NLNt6SC1PDGEnG2fw0fkDH4",quantity:1}]
+        }
+      )
+     })}
+    >Buy This image</button>
     </div>
   </main>
   )
